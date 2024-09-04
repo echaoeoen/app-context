e2158dd78311ccb3cfe7d4fbf6925067f6b19c86
echaoeoen <andyepso@gmail.com>
2024-09-04 21:34:53 +0700

    feat: initial release of AppContext library v1.0.0
    This is the first major release of the AppContext library. The library simplifies context management across asynchronous function calls in Node.js, using AsyncLocalStorage. It allows setting and retrieving context data without passing parameters through multiple function calls.
    
    Features:
    - Start a new execution context with `startContext`.
    - Retrieve context data with `get`.
    - Set or update context data with `set`.

---
+++ release.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    branches: ['master'],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      [
+        '@semantic-release/changelog',
+        {
+          changelogFile: 'CHANGELOG.md',
+        },
+      ],
+      '@semantic-release/github',
+      [
+        '@semantic-release/git',
+        {
+          assets: ['CHANGELOG.md', 'package.json'],
+          message:
+            // eslint-disable-next-line no-template-curly-in-string
+            'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        },
+      ],
+    ],
+    verifyConditions: ['@semantic-release/changelog', '@semantic-release/git'],
+  };
+  
